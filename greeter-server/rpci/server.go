637260145b4577d9c0ea3795c6f26cd2976fff10
Artem Karpov <ar.karpov@gmail.com>
2020-11-09 13:37:09 -0800

    make tilt reusable for different services

--- greeter-server/rpci/server.go
+++ greeter-server/rpci/server.go
@@ -14,12 +14,12 @@ const (
 	port = ":9000"
 )
 
-// server is used to implement helloworld.GreeterServer.
+// server is used to implement service.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer
 }
 
-// SayHello implements helloworld.GreeterServer
+// SayHello implements service.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
